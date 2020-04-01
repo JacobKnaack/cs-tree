a3f3c8c59f40b9024fb7cf68f096a6ac374aa88f
JacobKnaack <jacobknck@gmail.com>
2020-04-01 11:19:11 -0700

    adds dependency injection

--- Tree/tree.cs
+++ Tree/tree.cs
@@ -64,19 +64,4 @@ namespace Tree
       }
     }
   }
-
-  class Program
-  {
-    public static void Main()
-    {
-      System.Console.WriteLine("Main program run");
-      BinaryTree tree = new BinaryTree();
-      tree.insert(tree.r, 1);
-      tree.insert(tree.r, 10);
-      tree.insert(tree.r, 5);
-      tree.insert(tree.r, 6);
-      tree.insert(tree.r, 7);
-      tree.traverseIterativly(tree.r);
-    }
-  }
 }
